282792d42402d0ffabeba9c83308d38f7267525c
nolannish <nolannish@gmail.com>
2025-03-24 23:23:08 -0700

    Updated scripts to keep track of lap number and end scene on finish of lap 3

--- Assets/Scripts/RacingHandler.cs
+++ Assets/Scripts/RacingHandler.cs
@@ -2,20 +2,26 @@ using UnityEngine;
 
 public class RacingHandler : MonoBehaviour
 {
+
+    private int laps = 1;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger entered by: " + other.gameObject.name);
         
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player detected - finishing race");
-            // Find the RacingUI and call FinishRace
-            // TODO: Store time for leaderboard
-            RacingUI racingUI = FindAnyObjectByType<RacingUI>();
-            if (racingUI != null)
-            {
-                racingUI.FinishRace();
+            Debug.Log("Started Lap " + laps);
+            if(laps > 3){
+                Debug.Log("Player detected - finishing race");
+                // Find the RacingUI and call FinishRace
+                // TODO: Store time for leaderboard
+                RacingUI racingUI = FindAnyObjectByType<RacingUI>();
+                if (racingUI != null)
+                {
+                    racingUI.FinishRace();
+                }
             }
+            laps++;
         }
     }
 }
