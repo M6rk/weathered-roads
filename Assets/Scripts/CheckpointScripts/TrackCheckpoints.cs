091a6dad022f252bb8e9bcd37adfeb4cd12222b4
nolannish <nolannish@gmail.com>
2025-03-24 23:07:06 -0700

    Basic checkpoint system introduced with logic for keeping track of lap number
    Checkpoint system to ensure users follow the correct path of the track, and also keeps track of the lap number that is going to be set.

---
+++ Assets/Scripts/CheckpointScripts/TrackCheckpoints.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackCheckpoints : MonoBehaviour
+{
+    private List<CheckpointSingle> checkpointSingleList;
+    private int nextCheckpointSingleIndex;
+
+    public int laps;
+    private int numOfCheckpoints;
+    private int tempLapCounter;
+    private void Awake()
+    {
+        Transform checkpointsTransform = transform.Find("CheckPoints");
+        checkpointSingleList = new List<CheckpointSingle>();
+        foreach(Transform checkpointSingleTransform in checkpointsTransform){
+            CheckpointSingle checkpointSingle = checkpointSingleTransform.GetComponent<CheckpointSingle>();
+            checkpointSingle.SetTrackCheckpoints(this);
+
+            checkpointSingleList.Add(checkpointSingle);
+        }
+        numOfCheckpoints = checkpointSingleList.Count;
+        nextCheckpointSingleIndex = 0;
+        laps = 1;
+        tempLapCounter = laps;
+    }
+
+    public void PlayerThroughCheckpoint(CheckpointSingle checkpointSingle){
+        if(checkpointSingleList.IndexOf(checkpointSingle) == nextCheckpointSingleIndex){
+            //correct checkpoint
+            Debug.Log("Correct");
+            Debug.Log(laps);
+            // introduce logic for increasing the lap counter before the check of if its the right checkpoint.
+            // Reason for this is that want to update lap on index 0, but have the check be for the max index, so have it update lap counter on the next run through
+            if(tempLapCounter != laps){
+                tempLapCounter = laps;
+                Debug.Log("increase lap ui here");
+            }
+
+            if((checkpointSingleList.IndexOf(checkpointSingle) + 1) == numOfCheckpoints){
+                Debug.Log("increase");
+                IncreaseLaps();
+            }
+            
+            nextCheckpointSingleIndex = (nextCheckpointSingleIndex + 1) % checkpointSingleList.Count;
+        } else {
+            Debug.Log("wrong");
+            // incorrect checkpoing
+        }
+    }
+
+    public void IncreaseLaps(){
+        laps++;
+    }
+}
