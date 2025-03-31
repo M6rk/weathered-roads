3949976d01367ab9f641ac5b708d73b574150320
nolannish <nolannish@gmail.com>
2025-03-31 11:19:20 -0700

    Updated cc speed selection to be easier to control

--- Assets/Scripts/ModeSelection.cs
+++ Assets/Scripts/ModeSelection.cs
@@ -54,13 +54,13 @@ public class ModeSelection : MonoBehaviour
         selectedCC = ccValue;
         // Update button 
         if(selectedCC == 50){
-             VariableManager.instance.SetCCValue(1);
+             VariableManager.instance.SetCCValue(0.75f);
          }
          if(selectedCC == 100){
-             VariableManager.instance.SetCCValue(1.5f);
+             VariableManager.instance.SetCCValue(1f);
          }
          if(selectedCC == 150){
-             VariableManager.instance.SetCCValue(2);
+             VariableManager.instance.SetCCValue(1.25f);
          }
         SetButtonSelected(button);
         Debug.Log($"Selected CC: {selectedCC}");
