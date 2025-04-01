6298a6f9fb346e135d7381d88c809a6d611a8586
Harper Kerstens <63266571+harperkerstens@users.noreply.github.com>
2025-03-31 17:14:23 -0700

    click sounds + volume slider

--- Assets/Scripts/menuMusic.cs
+++ Assets/Scripts/menuMusic.cs
@@ -1,19 +1,63 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;  // Add this for scene management
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems; 
+using UnityEngine.UI; 
 
 public class menuMusic : MonoBehaviour
 {
+    [Header("UI Sounds")]
+    [SerializeField] public AudioSource menuSoundSource; 
+    [SerializeField] private AudioClip hoverSound;
+    [SerializeField] private AudioClip clickSound;
+    [SerializeField] private AudioClip invalidClickSound; 
 
-    private void Awake(){
-  
-           DontDestroyOnLoad(this.gameObject); // This will make sure the object is not destroyed when loading a new scene
-        
+    [Header("Volume Control")]
+    [SerializeField] private float masterVolume = 1f;
+    private AudioSource[] allAudioSources;
+
+    private GameObject lastHoveredButton;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void PlayHoverSound()
+    {
+        if (menuSoundSource != null && hoverSound != null)
+        {
+            menuSoundSource.PlayOneShot(hoverSound,3f);
+        }
+    }
+
+    public void PlayClickSound()
+    {
+        if (menuSoundSource != null && clickSound != null)
+        {
+            menuSoundSource.PlayOneShot(clickSound,3f);
+        }
+    }
+
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey("MasterVolume"))
+        {
+            masterVolume = PlayerPrefs.GetFloat("MasterVolume");
+            UpdateMasterVolume(masterVolume);
+        }
+    }
+
+    public void UpdateMasterVolume(float volume)
+    {
+        masterVolume = volume;
+        PlayerPrefs.SetFloat("MasterVolume", masterVolume);
+        PlayerPrefs.Save();
+
+        allAudioSources = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource source in allAudioSources)
+        {
+            source.volume = masterVolume;
+        }
     }
 
     // Update is called once per frame
@@ -23,6 +67,65 @@ public class menuMusic : MonoBehaviour
         if (currentScene == "TrackOne" || currentScene == "TrackTwo" || currentScene == "TrackThree")
         {
             Destroy(gameObject);
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0) && EventSystem.current != null)
+        {
+            PointerEventData pointerData = new PointerEventData(EventSystem.current)
+            {
+                position = Input.mousePosition
+            };
+
+            var results = new System.Collections.Generic.List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+
+            bool clickedButton = false;
+            foreach (var result in results)
+            {
+                if (result.gameObject.CompareTag("button"))
+                {
+                    PlayClickSound();
+                    clickedButton = true;
+                    break;
+                }
+            }
+
+            if (!clickedButton && menuSoundSource != null && invalidClickSound != null)
+            {
+                menuSoundSource.PlayOneShot(invalidClickSound, 2f);
+            }
+        }
+
+        if (EventSystem.current != null)
+        {
+            PointerEventData pointerData = new PointerEventData(EventSystem.current)
+            {
+                position = Input.mousePosition
+            };
+
+            var results = new System.Collections.Generic.List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+
+            GameObject currentHoveredObject = null;
+            foreach (var result in results)
+            {
+                if (result.gameObject.CompareTag("button"))
+                {
+                    currentHoveredObject = result.gameObject;
+                    break;
+                }
+            }
+
+            if (currentHoveredObject != null && currentHoveredObject != lastHoveredButton)
+            {
+                PlayHoverSound();
+                lastHoveredButton = currentHoveredObject;
+            }
+            else if (currentHoveredObject == null)
+            {
+                lastHoveredButton = null;
+            }
         }
     }
 }
